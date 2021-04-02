81af50faff5d84e4c3f855fd68e511dfe1899af7
Rowvin <7840167+rowvinn@users.noreply.github.com>
2021-04-02 15:07:21 -0700

    Added notes to the exercise to make it readable and easier to understand.

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -14,15 +14,29 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
 class Solution {
+    // Create a function that takes in two sorted Linked Lists and merge them together
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
+        // If either lists are empty, return the non-empty one. Otherwise, just return the contents of the second LinkedList.
+        if(l1 == null){
             return l2;
         }
-        else if (l2 == null) {
+        else if(l2 == null){
             return l1;
         }
-        else if (l1.val < l2.val) {
+        // Since the lists are sorted, we can compare the #'s in both lists and set the smaller one as the next node in the LinkedList until we go through either list one or two. Then we return the list.
+        else if(l1.val < l2.val){
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         }
